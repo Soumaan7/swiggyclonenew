fedf8e2949f3ea1004af71f72e3c5a97705a867a
Soumaan7 <beighsumaan@gmail.com>
2025-03-05 02:02:32 +0530

    header section done

--- main.jsx
+++ main.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="header">
+      <div className="img-container">
+        <img
+          className="logo"
+          src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo.png"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 const AppLayout = () => {

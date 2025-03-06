0759bce8a420d6cd1d11bc8586ebb80c18c86c9e
Soumaan7 <beighsumaan@gmail.com>
2025-03-06 16:45:28 +0530

    Filtered Button done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,19 @@
 import ResCard from "./ResCard";
 import resList from "../utils/mockData";
+let listOfRes = resList;
 const Body = () => {
   return (
     <div>
-      <div className="search-container">Search</div>
+      <div className="filter">
+        <button
+          onClick={() => {
+            listOfRes = listOfRes.filter((res) => res.info.avgRating > 4.5);
+            console.log(listOfRes);
+          }}
+        >
+          Top rated restaurants
+        </button>
+      </div>
       <div className="res-container">
         {resList.map((data) => (
           <ResCard key={data.info.id} resData={data} />

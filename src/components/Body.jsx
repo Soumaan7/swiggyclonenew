d110aaa88b35b1c4d4abf25ecb7d66aade059fb1
Soumaan7 <beighsumaan@gmail.com>
2025-03-06 17:49:56 +0530

    filtered List using useState

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,21 +1,25 @@
 import ResCard from "./ResCard";
 import resList from "../utils/mockData";
-let listOfRes = resList;
+import { useState } from "react";
+
 const Body = () => {
+  const [listOfRes, setListOfRes] = useState(resList);
   return (
     <div>
       <div className="filter">
         <button
           onClick={() => {
-            listOfRes = listOfRes.filter((res) => res.info.avgRating > 4.5);
-            console.log(listOfRes);
+            const filterRes = listOfRes.filter(
+              (res) => res.info.avgRating > 4.4
+            );
+            setListOfRes(filterRes);
           }}
         >
           Top rated restaurants
         </button>
       </div>
       <div className="res-container">
-        {resList.map((data) => (
+        {listOfRes.map((data) => (
           <ResCard key={data.info.id} resData={data} />
         ))}
       </div>

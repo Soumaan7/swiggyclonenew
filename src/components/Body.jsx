5d412bcecf894e71689cece9f6827fd00a2b2e65
Soumaan7 <beighsumaan@gmail.com>
2025-03-08 00:54:43 +0530

    Shimmer UI done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,26 +1,31 @@
 import ResCard from "./ResCard";
-
 import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
+const fetchdata = async () => {
+  const data = await fetch(
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  );
+  const json = await data.json();
+  console.log(json);
+  const restaurants =
+    json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
+  setListOfRes(restaurants);
+  setDefaultList(restaurants);
+};
 
 const Body = () => {
   const [listOfRes, setListOfRes] = useState([]);
   const [defaultList, setDefaultList] = useState([]);
   const [isFiltered, setIsFiltered] = useState(false);
+
   useEffect(() => {
     fetchdata();
   }, []);
-  const fetchdata = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
-    const json = await data.json();
-    console.log(json);
-    const restaurants =
-      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
-    setListOfRes(restaurants);
-    setDefaultList(restaurants);
-  };
-  return (
+
+  return listOfRes.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div>
       <div style={{ display: "flex" }}>
         <div className="filter">

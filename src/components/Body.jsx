7e9947b707661e609307514fb483183346e121a1
Soumaan7 <beighsumaan@gmail.com>
2025-03-07 14:52:01 +0530

    default filter button

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,20 +4,33 @@ import { useState } from "react";
 
 const Body = () => {
   const [listOfRes, setListOfRes] = useState(resList);
+  const [defaultList] = useState(resList);
   return (
     <div>
-      <div className="filter">
-        <button
-          onClick={() => {
-            const filterRes = listOfRes.filter(
-              (res) => res.info.avgRating > 4.4
-            );
-            setListOfRes(filterRes);
-          }}
-        >
-          Top rated restaurants
-        </button>
+      <div>
+        <div className="filter">
+          <button
+            onClick={() => {
+              const filterRes = listOfRes.filter(
+                (res) => res.info.avgRating > 4.4
+              );
+              setListOfRes(filterRes);
+            }}
+          >
+            Top rated restaurants
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setListOfRes(defaultList);
+            }}
+          >
+            X
+          </button>
+        </div>
       </div>
+
       <div className="res-container">
         {listOfRes.map((data) => (
           <ResCard resData={data} />

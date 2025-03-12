86c00281b4e6f84b479732c74a56b498ba21b11a
Soumaan7 <beighsumaan@gmail.com>
2025-03-12 18:35:01 +0530

    dynamic routing done

--- src/components/Resmenu.jsx
+++ src/components/Resmenu.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_API } from "../utils/constants";
 
 const Resmenu = () => {
+  const { resId } = useParams();
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const [menuInfo, setMenuInfo] = useState(null);
+
   const fetchMenu = async () => {
     const resInfo = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=10575&catalog_qa=undefined&submitAction=ENTER"
+      MENU_API + resId + "&catalog_qa=undefined&submitAction=ENTER"
     );
     let jsonData = await resInfo.json();
     console.log(jsonData);
@@ -29,11 +33,14 @@ const Resmenu = () => {
       <h3>MENU</h3>
       <ul>
         {itemCards.map((res, index) => (
-          <li key={index}>{res.card.info.name}</li>
+          <li key={index}>
+            Rs. {res.card.info.name}- Rs.
+            {res.card.info.price / 100 || res.card.info.defaultPrice / 100}{" "}
+          </li>
         ))}
       </ul>
-      <h1>{itemCards[0].card.info.name}</h1>
-      {/* <h3>{costForTwoMessage}</h3> */}
+      {/* <h1>{itemCards[0].card.info.name}</h1>
+      <h3>{costForTwoMessage}</h3> */}
     </div>
   );
 };
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -2,3 +2,5 @@ export const LOGO_URL =
   "https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo.png";
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";

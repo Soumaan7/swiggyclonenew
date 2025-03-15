2726003c0fb5d9843b58baf725d46062815e4133
Soumaan7 <beighsumaan@gmail.com>
2025-03-16 01:25:23 +0530

    creating state variables in class based component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const User = (props) => {
+  const [count] = useState(0);
   return (
     <div>
       <h1>Name: {props.name}</h1>
+      <h2>{count}</h2>
     </div>
   );
 };
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -2,11 +2,17 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 1,
+      count1: 2,
+    };
   }
   render() {
     return (
       <div>
         <h1>{this.props.name}</h1>
+        <h2>{this.state.count}</h2>
+        <h2>{this.state.count1}</h2>
       </div>
     );
   }

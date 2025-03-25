5a3378b6ac8e604f2eb00564a0dda241dbe46952
Soumaan <beighsumaan@gmail.com>
2025-03-25 15:29:33 +0530

    online offline status

--- src/utils/useOnlineStatus.jsx
+++ src/utils/useOnlineStatus.jsx
@@ -1,1 +1,15 @@
+import { useEffect, useState } from "react";
 
+const useOnlineStatus = () => {
+  const [onlineStatus, setOnline] = useState(true);
+  useEffect(() => {
+    window.addEventListener("online", () => {
+      setOnline(true);
+    });
+    window.addEventListener("offline", () => {
+      setOnline(false);
+    });
+  }, []);
+  return onlineStatus;
+};
+export default useOnlineStatus;
